0dc9a01d59a20d48961c4a88b76c7ee190abc012
juhee4839 <79627080+juhee4839@users.noreply.github.com>
2021-07-19 16:13:25 +0900

    markup.html

---
+++ js/intro.js
@@ -0,0 +1,10 @@
+/*const intro_click = document.querySelector(".intro__click__column > span");
+const intro_bar = document.querySelector(".intro_bar__stack_link");
+
+
+function handleClick(e){
+    e.preventDefault();
+    intro_bar.classList.toggle("hidden");
+}
+
+intro_click.addEventListener("click",handleClick);*/
