0e0b7e5154df15625590eb159edea03a41b8e055
juhee4839 <79627080+juhee4839@users.noreply.github.com>
2021-07-22 18:10:26 +0900

    Update intro.js

--- js/intro.js
+++ js/intro.js
@@ -9,6 +9,9 @@ function handleClick(e){
 
 intro_click.addEventListener("click",handleClick);*/
 
+/*
+
+<<<<<<< HEAD
 const smImg = document.querySelectorAll("#smImg > li");
 const lgImg = document.querySelectorAll("#lgImg > li");
 
@@ -18,3 +21,21 @@ smImg.forEach(sImg => sImg.addEventListener("click",function(){
 function handdleClick(){
     
 }
+=======
+const smImgs = document.querySelectorAll("#smImg > li");
+const lgImgs = document.querySelectorAll("#lgImg > li");
+
+smImgs.forEach(smImg => smImg.addEventListener("click",function(){
+    for(let i=0;i<smImgs.length; i++)smImgs[i].classList.remove("on")
+    smImg.classList.add("on");
+    let index = getElementIndex(smImgs, smImg);
+    console.log(index)
+    for(let i=0;i<lgImgs.length; i++)lgImgs[i].classList.remove("on")
+    lgImgs[index].classList.add("on");
+}))
+function getElementIndex(element, range) {//요소의 index번호를 구하는 함수
+    return [].indexOf.call(element, range);
+    //return [].indexOf.call(element.parentNode.children, element);
+}
+>>>>>>> 25ed42633edf3b0c396945e512abfa1f596747e2
+*/
